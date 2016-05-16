2464b6f1a9f15e65385210c8b55e3e9b33a4172c
harttle <yangjvn@126.com>
2016-05-17 02:02:42 +0800

    set status in error handler

--- bricks/error/router.js
+++ bricks/error/router.js
@@ -1,7 +1,10 @@
 
-exports.get = function(req, done, fail){
+exports.get = function(req, done, fail, res){
     var err = this.error;
+    res.status(err.status || 500);
+
     console.error(err);
+
     done({
         message: err.message || err,
         stack: err.stack
